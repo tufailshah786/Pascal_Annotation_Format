1a87626587b64afbc974346019e90cb04799afbf
Tufail Sajjad Shah Hashmi <69716088+tufailshah786@users.noreply.github.com>
2020-08-17 15:30:17 +0500

    Update allangle.py

--- Pascal_Format/allangle.py
+++ Pascal_Format/allangle.py
@@ -4,10 +4,10 @@ import math
 import os
 import xml.etree.ElementTree as ET
 i=0
-for filename in os.listdir("C:/python/allangletest/"): 
+for filename in os.listdir("C:/Pascal_Annotation_Format-master/Pascal_Format/results/"): 
     filename=os.path.splitext(os.path.basename(filename))[0]
     print(filename)
-    tree = ET.parse("C:/python/allangletest/"+filename+'.xml')
+    tree = ET.parse("C:/Pascal_Annotation_Format-master/Pascal_Format/results/"+filename+'.xml')
     root = tree.getroot()
     for objects in tree.iter('object'):
       #robndbox = objects.find('robndbox')
@@ -23,10 +23,10 @@ for filename in os.listdir("C:/python/allangletest/"):
         ymin = (yc-(height)/2)
         xmax= (xc+(width/2))
         ymax = (yc+(height)/2)
-        print(xmin)
-        print(ymin)
-        print(xmax)
-        print(ymax)
+        #print(xmin)
+        #print(ymin)
+        #print(xmax)
+        #print(ymax)
        for doc in objects.findall('robndbox/cx'):
            doc.tag="xmin"
            doc.text=str(xmin)
@@ -39,6 +39,6 @@ for filename in os.listdir("C:/python/allangletest/"):
        for doc in objects.findall('robndbox/h'):
            doc.tag="ymax" 
            doc.text=str(ymax)           
-    tree.write("C:/python/allangletest/"+filename+'.xml')
+    tree.write("C:/Pascal_Annotation_Format-master/Pascal_Format/results/"+filename+'.xml')
         
     i +=1
